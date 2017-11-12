331b4109e3fc50ac02fcc6c96df65dbc59344618
yabecchi <yabecchi.ext@gmail.com>
2017-11-12 11:32:42 +0900

    RxSwift Adaptation

--- Likes/Models/LikesListModel.swift
+++ Likes/Models/LikesListModel.swift
@@ -6,8 +6,49 @@
 //  Copyright © 2017年 ExtYabecchi. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+import TwitterKit
 
 struct LikesListModel: Codable {
-    
+    let test1: Double
+    let test2: Double
+    init(_ test1: Double, _ test2: Double) {
+        self.test1 = test1
+        self.test2 = test2
+    }
+    static var dummyData: LikesListModel {
+        return LikesListModel(1.0, 2.0)
+    }
+}
+
+extension LikesListModel {
+    func request() {
+        guard let twitterAccountId = Twitter.sharedInstance().sessionStore.session()?.userID else { return }
+        
+        let client = TWTRAPIClient(userID: twitterAccountId)
+        var clientError : NSError?
+        
+        let request = client.urlRequest(
+            withMethod: "GET",
+            url: "https://api.twitter.com/1.1/favorites/list.json",
+            parameters: [
+                "user_id": twitterAccountId,
+                "count": "10",
+                ],
+            error: &clientError
+        )
+        
+        client.sendTwitterRequest(request) { (response, data, connectionError) -> Void in
+            if connectionError != nil {
+                print("Error: \(connectionError)")
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                print("json: \(json)")
+            } catch let jsonError as NSError {
+                print("json error: \(jsonError.localizedDescription)")
+            }
+        }
+    }
 }
--- Likes/Views/Controllers/LikesListViewController.swift
+++ Likes/Views/Controllers/LikesListViewController.swift
@@ -7,17 +7,52 @@
 //
 
 import UIKit
+import TwitterKit
+import RxSwift
+import RxCocoa
+import RxDataSources
 
-class LikesListViewController: UIViewController {
-
+class LikesListViewController: UIViewController, UITableViewDelegate {
+    @IBOutlet var tableView: UITableView!
+    
+    private var viewModel: LikesListViewModel!
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        viewModel = LikesListViewModel()
+        
+        viewModel.items
+            .bind(to: tableView.rx.items(dataSource: viewModel.dataSource))
+            .disposed(by: disposeBag)
+        
+        tableView.rx
+            .itemSelected
+            .map { indexPath in
+                return (indexPath, self.viewModel.dataSource[indexPath])
+            }
+            .subscribe(onNext: { indexPath, model in
+                
+            })
+            .disposed(by: disposeBag)
+        
+        tableView.rx
+            .setDelegate(self)
+            .disposed(by: disposeBag)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return .none
     }
-
-
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
 }
+
+
+//extension LikesListViewController: UITableViewDelegate {
+//
+//}
 
