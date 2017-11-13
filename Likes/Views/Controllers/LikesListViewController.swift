7c67c08023c4b0e76e7aeaceda206c0b0d53dbc0
yabecchi <yabecchi.ext@gmail.com>
2017-11-14 07:30:09 +0900

    複数section分けから単一セクションに変更

--- Likes/Views/Controllers/LikesListViewController.swift
+++ Likes/Views/Controllers/LikesListViewController.swift
@@ -12,7 +12,7 @@ import RxSwift
 import RxCocoa
 import RxDataSources
 
-class LikesListViewController: UIViewController, UITableViewDelegate {
+class LikesListViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     
     private var viewModel: LikesListViewModel!
@@ -22,26 +22,44 @@ class LikesListViewController: UIViewController, UITableViewDelegate {
         super.viewDidLoad()
         
         viewModel = LikesListViewModel()
-        
         viewModel.items
-            .bind(to: tableView.rx.items(dataSource: viewModel.dataSource))
-            .disposed(by: disposeBag)
-        
-        tableView.rx
-            .itemSelected
-            .map { indexPath in
-                return (indexPath, self.viewModel.dataSource[indexPath])
+            .bind(to: tableView.rx.items(cellIdentifier: "cell", cellType: UITableViewCell.self)) { (row, element, cell) in
+                cell.textLabel?.text = "\(element.test1) @ row \(row)"
             }
-            .subscribe(onNext: { indexPath, model in
-                
-            })
             .disposed(by: disposeBag)
         
         tableView.rx
             .setDelegate(self)
             .disposed(by: disposeBag)
+        
+//        let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, LikesListModel>>(
+//            configureCell: { (dataSource, table, indexPath, element) in
+//                let cell = table.dequeueReusableCell(withIdentifier: "cell")!
+//                cell.textLabel?.text = "\(element.test1) @ row \(indexPath.row)"
+//                return cell
+//            }
+//        )
+//        dataSource.titleForHeaderInSection = { dataSource, sectionIndex in
+//            return dataSource[sectionIndex].model
+//        }
+//        viewModel.items
+//            .bind(to: tableView.rx.items(dataSource: viewModel.dataSource))
+//            .disposed(by: disposeBag)
+        
+//        tableView.rx
+//            .itemSelected
+//            .map { indexPath in
+//                return (indexPath, self.viewModel.dataSource[indexPath])
+//            }
+//            .subscribe(onNext: { indexPath, model in
+//
+//            })
+//            .disposed(by: disposeBag)
     }
-    
+}
+
+
+extension LikesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
         return .none
     }
@@ -50,9 +68,4 @@ class LikesListViewController: UIViewController, UITableViewDelegate {
         return 40
     }
 }
-
-
-//extension LikesListViewController: UITableViewDelegate {
-//
-//}
 
